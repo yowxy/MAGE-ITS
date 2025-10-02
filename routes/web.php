07c098361707684c389b186b9dfdbec36e75fc5d
mcDJIL <mdjauharil29@gmail.com>
2025-10-02 22:16:43 +0700

    feat: redesign login and register

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,7 @@ Route::get('/', function () {
 Route::get('/login', function () {
     return view('livewire.auth.login');
 });
+
+Route::get('/register', function() {
+    return view('livewire.auth.register');
+});
